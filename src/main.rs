50885e780fa847983a9c0bb0591da664078d16ce
Nathan Jhaveri <nathanj@porch.com>
2021-07-26 09:53:30 -0700

    Add color_eyre

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
+use color_eyre::Report;
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Report> {
+    setup()?;
+
     println!("Hello, world!");
+
+    Ok(())
+}
+
+fn setup() -> Result<(), Report> {
+    if std::env::var("RUST_LIB_BACKTRACE").is_err() {
+        std::env::set_var("RUST_LIB_BACKTRACE", "1");
+    }
+
+    color_eyre::install()?;
+
+    Ok(())
 }
