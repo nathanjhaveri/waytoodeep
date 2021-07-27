040718858797f7a7bbf7a6894415fd65724066de
Nathan Jhaveri <nathanj@porch.com>
2021-07-27 09:26:39 -0700

    explicit function sig

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,40 @@
+#![allow(dead_code)]
+
 use color_eyre::Report;
+use tracing::info;
+use tracing_subscriber::EnvFilter;
+use reqwest::Client;
+use std::future::Future;
+use std::time::Duration;
+use std::sync::Arc;
+
+pub const URL_1: &str = "https://jhaveri.net";
+pub const URL_2: &str = "https://fasterthanli.me";
 
 #[tokio::main]
 async fn main() -> Result<(), Report> {
     setup()?;
 
-    println!("Hello, world!");
+    info!("Hi from the program");
+    let client = Client::new();
+    let client = Arc::new(client);
+
+    let fut1 = fetch(client.clone(), URL_1);
+    tokio::spawn(fut1);
+    let fut2 = fetch(client.clone(), URL_2);
+    tokio::spawn(fut2);
+
+
+    //fut1.await?;
+    //fut2.await?;
+
+    tokio::time::sleep(Duration::from_secs(1)).await;
 
     Ok(())
+}
+
+fn type_name_of<T>(_: &T) -> &str {
+    std::any::type_name::<T>()
 }
 
 fn setup() -> Result<(), Report> {
@@ -16,5 +44,21 @@ fn setup() -> Result<(), Report> {
 
     color_eyre::install()?;
 
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "info")
+    }
+    tracing_subscriber::fmt::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
     Ok(())
+}
+
+fn fetch(client: Arc<Client>, url: &'static str) -> impl Future<Output=Result<(), Report>> + 'static {
+    async move {
+        let res = client.get(url).send().await?.error_for_status()?;
+        info!(%url, content_type = ?res.headers().get("content-type"), "Got a response");
+
+        Ok(())
+    }
 }
