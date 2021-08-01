5251c8fa63c4c8affe9a70a939fdfc1e93c501f8
Nathan Jhaveri <jhaveri@umich.edu>
2021-07-31 21:49:47 -0700

    working try_join, concurrent

--- src/tj.rs
+++ src/tj.rs
@@ -1,11 +1,18 @@
-use std::{future::Future, pin::Pin, task::Poll, task::Context};
+use std::{future::Future, pin::Pin, task::Context, task::Poll};
+
+use tracing::info;
 
 pub fn try_join<A, B, AR, BR, E>(a: A, b: B) -> impl Future<Output = Result<(AR, BR), E>>
 where
     A: Future<Output = Result<AR, E>>,
     B: Future<Output = Result<BR, E>>,
 {
-    TryJoin { a, b }
+    TryJoin {
+        a,
+        b,
+        a_res: None,
+        b_res: None,
+    }
 }
 struct TryJoin<A, B, AR, BR, E>
 where
@@ -14,6 +21,9 @@ where
 {
     a: A,
     b: B,
+
+    a_res: Option<AR>,
+    b_res: Option<BR>,
 }
 
 impl<A, B, AR, BR, E> Future for TryJoin<A, B, AR, BR, E>
@@ -23,7 +33,52 @@ where
 {
     type Output = Result<(AR, BR), E>;
 
-    fn poll(self: Pin<&mut Self>, _: &mut Context<'_>) -> Poll<Self::Output> {
-        todo!()
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let this = unsafe { self.get_unchecked_mut() };
+        let (a, b) = unsafe {
+            (
+                Pin::new_unchecked(&mut this.a),
+                Pin::new_unchecked(&mut this.b),
+            )
+        };
+
+        if this.a_res.is_none() {
+            match a.poll(cx) {
+                Poll::Ready(a_res) => match a_res {
+                    Ok(a) => {
+                        info!("a is ready!");
+                        this.a_res = Some(a);
+                    }
+                    Err(a_error) => return Poll::Ready(Err(a_error)),
+                },
+                Poll::Pending => {
+                    info!("a is pending");
+                }
+            }
+        }
+
+        if this.b_res.is_none() {
+            match b.poll(cx) {
+                Poll::Ready(res) => match res {
+                    Ok(b) => {
+                        info!("b is ready!");
+                        this.b_res = Some(b);
+                    }
+                    Err(e) => return Poll::Ready(Err(e)),
+                },
+                Poll::Pending => {
+                    info!("b is pending");
+                }
+            }
+        }
+
+        if this.a_res.is_some() && this.b_res.is_some() {
+            let a = this.a_res.take().unwrap();
+            let b = this.b_res.take().unwrap();
+            Poll::Ready(Ok((a, b)))
+        } else {
+            Poll::Pending
+        }
+
     }
 }
